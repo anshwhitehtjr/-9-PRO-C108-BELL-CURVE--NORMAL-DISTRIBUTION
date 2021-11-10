012a7460572aec879d4fe60e36a842fa0b778088
anshwhitehtjr <kalpaiteansh@gmail.com>
2021-11-10 13:29:07 +0530

    Files

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import plotly.figure_factory as ff
+import scipy
+
+df = pd.read_csv("data.csv")
+mean = df["Avg Rating"].mean()
+
+fig = ff.create_distplot([df["Avg Rating"]], ["Demo Name"], show_hist=False)
+fig.show()
